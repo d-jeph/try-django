f340d6169f0736391fd561d5d983261dcaefd04c
d-jeph <jephdavid@gmail.com>
2017-03-19 17:38:28 +0300

    Added templeate inheritance and django forms

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,33 @@
-from django.http import HttpResponse
-from django.shortcuts import render,get_object_or_404
-from  .models import Post
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import messages
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Post
+from .forms import PostForm
 # Create your views here.
+
 def post_list(request):
     queryset= Post.objects.all();
     context_data ={
     "post_list" : queryset,
     "title" : "List"
     }
-    return render(request,"index.html",context_data);
+    return render(request,"posts.html",context_data);
+
 def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request,"Post created successfully")
+        return HttpResponseRedirect(instance.get_absolute_url())
 
-    return HttpResponse("<h1>Create</h1>");
+    context_data = {
+        "form":form,
+        "title" : "New Post",
+    }
+    return render(request,"post_form.html",context_data);
 
-def post_detail(request,id):
+def post_detail(request,id=None):
     post = get_object_or_404(Post,id=id)
     context_data ={
     "post": post,
@@ -22,9 +36,27 @@ def post_detail(request,id):
     return render(request,"detail.html",context_data);
 
 
-def post_update(request):
+def post_update(request,id=None):
+    instance = get_object_or_404(Post,id=id)
+    form = PostForm(request.POST or None,instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        #add some messag here
+        messages.success(request,"Post saved")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    # else:
+    #     messages.success(request,"An error occured")
+ 
+    context_data ={
+    "post": instance,
+    "title" : instance.title,
+    "form": form,
+    }
+    return render(request,"post_form.html",context_data);
 
-    return HttpResponse("<h1>update</h1>");
-def post_delete(request):
-
-    return HttpResponse("<h1>delete</h1>");
+def post_delete(request,id=None):
+    instance = get_object_or_404(Post,id=id)
+    instance.delete()
+    messages.success(request,"Post deleted")
+    return redirect("posts:list");
